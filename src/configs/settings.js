6c6a801ee57e24d81a16768a42e698565f3f86f4
Tu Nguyen <tuna9x.it@gmail.com>
2025-04-18 18:10:27 +0700

    Scaffold files

---
+++ src/configs/settings.js
@@ -0,0 +1,11 @@
+const settings = {
+  title: "Tu Nguyen",
+  description: "Tu Nguyen's blog",
+  url: "https://www.tunguyen.me",
+  email: "tuna9x.it@gmail.com",
+  facebook: "https://www.facebook.com/mrtu.vn",
+  linkedin: "https://www.linkedin.com/in/mrtuvn",
+  github: "https://github.com/mrtuvn",
+};
+
+export default settings;
---
+++ src/utils/course.ts
@@ -0,0 +1,16 @@
+import { getCollection } from "astro:content";
+
+export async function getCourseWorks(): Promise<any[]> {
+  const courses = await getCollection("course");
+  return courses.map((entry: any) => {
+    return {
+      id: entry.id,
+      title: entry.data.title,
+      description: entry.data.description,
+      img: entry.data.img,
+      img_alt: entry.data.img_alt || "",
+      path: entry.data.path || "",
+      caption: entry.data.caption || "",
+    };
+  });
+}
